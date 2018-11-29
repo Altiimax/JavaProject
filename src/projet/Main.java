78e437e72e3b67c17bdc3b028e3df9527bee59e1
celiaHassaine <44391501+celiaHassaine@users.noreply.github.com>
2018-11-29 20:31:07 +0100

    Update src/projet/Main.java

---
+++ src/projet/Main.java
@@ -0,0 +1,29 @@
+package projet;
+
+public class Main {
+
+    public static void main(String[] args) 
+    {
+
+        // Create the board we are going to play in /
+        Plateau origiBoard = new Plateau();
+
+        // Initiate a game with the board /
+        LeJeu aGame = new LeJeu (origiBoard);
+        aGame.getPlateau().initPlateau();
+
+        // Define the player /
+        Joueur firstPlayer = new Joueur("Zidane");
+        Joueur secondPlayer = new Joueur("Ronaldo");
+        Joueur thirdPlayer = new Joueur("Beckam");
+
+        aGame.addJoueur(firstPlayer);
+        aGame.addJoueur(secondPlayer);
+        aGame.addJoueur(thirdPlayer);
+
+        // Play 20 rounds 
+        //aGame.playManyTimes(20);
+
+    }
+
+}
