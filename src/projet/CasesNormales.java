c2d54905594a8a541c620c480e9683c9ece96463
Altiimax <44198671+Altiimax@users.noreply.github.com>
2018-11-29 18:13:30 +0100

    CasesNormales done

--- src/projet/CasesNormales.java
+++ src/projet/CasesNormales.java
@@ -1,57 +1,73 @@
 package projet;
 
 public class CasesNormales implements Case {
+	
+	protected int index;
+	protected Joueur joueur;
 
 	public CasesNormales(int i) {
-		// TODO Auto-generated constructor stub
+		this.index = i;
+		this.joueur = null;
+	}
+	
+	public CasesNormales(int i, Joueur bob) {
+		this.index = i;
+		this.joueur = bob;
 	}
 
 	@Override
+	/**
+	 * @return true because it is a normal cell, therefore, no special treatments for player on this cell
+	 */
 	public boolean peuxPartir() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public int getIndex() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.index;
 	}
 
 	@Override
+	/**
+	 * @return the index since this is a normal cell, we don't need the number of the dice throw for anything.
+	 */
 	public int mouvements(int lancerDé) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.getIndex();
 	}
 
 	@Override
 	public boolean caseOccupee() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.getJoueur() != null;
 	}
 
 	@Override
 	public void remplaceJoueur(Joueur joueur) {
-		// TODO Auto-generated method stub
-		
+		if (this.getJoueur() != null) {
+			System.out.println("Le joueur " + this.getJoueur().toString() + " est deja dans cette case !");
+			Joueur aRemplacer = this.getJoueur();
+			Case aEnvoyer = joueur.getCaseActuelle();
+			aRemplacer.setCaseActuelle(aEnvoyer);
+			System.out.println("Le joueur "+joueur.toString() + "vient d'arriver dans cette case et éjecte donc le joueur "
+			+aRemplacer.toString() + " à la case : " + aEnvoyer.getIndex());
+		}
 	}
 
 	@Override
 	public Joueur getJoueur() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.joueur;
 	}
 
 	@Override
 	public void setJoueur(Joueur joueur) {
-		// TODO Auto-generated method stub
-		
+		this.joueur = joueur;
 	}
 
 	@Override
+	/**
+	 * this method doesn't trigger itself on a normal cell.
+	 */
 	public void passeUnTour() {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
