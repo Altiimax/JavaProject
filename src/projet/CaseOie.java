f8bf62068f6f70df0063d78ca5a5a65b061c81d6
Altiimax <44198671+Altiimax@users.noreply.github.com>
2018-11-29 16:55:11 +0100

    All classes implemented

---
+++ src/projet/CaseOie.java
@@ -0,0 +1,57 @@
+package projet;
+
+public class CaseOie implements Case {
+
+	public CaseOie(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean peuxPartir() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mouvements(int lancerDé) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean caseOccupee() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void welcomeJoueur(Joueur joueur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Joueur getJoueur() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPlayer(Joueur joueur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void passeUnTour() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
