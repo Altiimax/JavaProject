8b1a3dfeaa6f67d130688200ec7e8b8c1378cc35
Altiimax <44198671+Altiimax@users.noreply.github.com>
2018-12-18 19:44:31 +0100

    RandoMPush

---
+++ projet/CaseAscenseur.java
@@ -0,0 +1,83 @@
+package projet;
+/**
+ * this class makes a player going from a specific cell to another and same in the other way
+ * @author celia, Kevin, Arnaud
+ *
+ */
+public class CaseAscenseur implements Case {
+	// current index of the player
+	protected int index;
+	// the player concerned by this case
+	protected Joueur joueur;
+	
+	/*
+	 * constructor of the CaseAscenceur knowing its index / 
+	 */
+	public CaseAscenseur(int i) {
+		this.index = i;
+		this.joueur = null;
+	}
+	
+	public CaseAscenseur(int i, Joueur joueur) {
+		this.index = i;
+		this.joueur = joueur;
+	}
+
+	@Override
+	public boolean peuxPartir() {
+		return true;
+	}
+
+	@Override
+	public int getIndex() {
+		return this.index;
+	}
+
+	@Override
+	public int mouvements(int lancerDe) {
+		if(this.joueur != null) {
+			int indexDeux = 0;
+			System.out.println("* Le joueur " + this.getJoueur() + " est entré dans un ascenseur !");
+			if(this.getIndex() == 8) { indexDeux = 16;}
+			if(this.getIndex() == 16) { indexDeux = 8;}
+			if(this.getIndex() == 45) { indexDeux = 62;}
+			if(this.getIndex() == 62) { indexDeux = 45;}
+			System.out.println("* L'ascenseur l'a emmené jusqu'à la case " + indexDeux);
+			return indexDeux;
+		} else {
+			return 0;
+		}
+	}
+
+	@Override
+	public boolean caseOccupee() {
+		return this.getJoueur() != null;
+	}
+
+	@Override
+	public void remplaceJoueur(Joueur joueur) {
+		if (this.getJoueur() != null) {
+			System.out.println("* Le joueur " + this.getJoueur().toString() + " est deja dans cette case !");
+			Joueur aRemplacer = this.getJoueur();
+			Case aEnvoyer = joueur.getCaseActuelle();
+			aRemplacer.setCaseActuelle(aEnvoyer);
+			System.out.println("* Le joueur "+joueur.toString() + " vient d'arriver dans cette case et éjecte donc le joueur "
+			+aRemplacer.toString() + " à la case : " + aEnvoyer.getIndex());
+		}
+	}
+
+	@Override
+	public Joueur getJoueur() {
+		return this.joueur;
+	}
+
+	@Override
+	public void setJoueur(Joueur joueur) {
+		this.joueur = joueur;
+	}
+
+	@Override
+	public void passeUnTour() {
+	}
+
+}
---
+++ projet/CasePrison.java
@@ -0,0 +1,89 @@
+package projet;
+
+public class CasePrison implements Case {
+	
+	protected int index;
+	protected Joueur joueur;
+	protected int nbToursEnPrison = 3;
+
+	public int getNbToursEnPrison() {
+		return nbToursEnPrison;
+	}
+
+	public void setNbToursEnPrison(int nbToursEnPrison) {
+		this.nbToursEnPrison = nbToursEnPrison;
+	}
+	
+	public CasePrison(int i) {
+		this.index = i;
+		this.nbToursEnPrison = 3;
+	}
+
+	/**
+	public CasePrison(int i, int nbreTours) {
+		this.index = i;
+		this.joueur = null;
+		this.nbToursEnPrison = nbreTours;
+	}
+	*/
+
+	@Override
+	public boolean peuxPartir() {
+		if(this.getNbToursEnPrison() == 0) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+
+	@Override
+	public int getIndex() {
+		return this.index;
+	}
+
+	@Override
+	public int mouvements(int lancerDe) {
+		System.out.println("* Le joueur " + this.getJoueur() + " a atteint une case prison !");
+		return this.index;
+	}
+
+	@Override
+	public boolean caseOccupee() {
+		return this.getJoueur() != null;
+	}
+
+	@Override
+	public void remplaceJoueur(Joueur joueur) {
+		
+	if (this.getJoueur() != null) {
+			System.out.println("* Le joueur " + this.getJoueur().toString() + " est deja dans cette case !");
+			Joueur aRemplacer = this.getJoueur();
+			Case aEnvoyer = joueur.getCaseActuelle();
+			aRemplacer.setCaseActuelle(aEnvoyer);
+			System.out.println("* Le joueur "+joueur.toString() + " vient d'arriver dans cette case et éjecte donc le joueur "
+			+aRemplacer.toString() + " à la case : " + aEnvoyer.getIndex());
+		}
+	}
+	
+	@Override
+	public Joueur getJoueur() {
+		return this.joueur;
+	}
+
+	@Override
+	public void setJoueur(Joueur joueur) {
+		this.joueur = joueur;
+	}
+
+	@Override
+	/**
+	 * Method used to deplement the number of turns the player has to wait while in this case
+	 */
+	public void passeUnTour() {
+		System.out.println("* Le joueur " + this.getJoueur() + " doit encore attendre : "+ this.getNbToursEnPrison() + 
+		" tours avant de pourvoir relancer le dé");
+		this.setNbToursEnPrison(this.getNbToursEnPrison() - 1);
+	}
+
+}
